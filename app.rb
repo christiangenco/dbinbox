49567bb918e3c34bcc5d23b45d7ba9b67e3f2342
Christian Genco <christian.genco@gmail.com>
2012-05-02 21:38:44 +0800

    switched gem to official and modeled it like https://www.dropbox.com/developers/reference/sdk

--- app.rb
+++ app.rb
@@ -1,30 +1,48 @@
+require 'sinatra'
+require 'dropbox_sdk'
 enable :sessions
 
-# Tutorial by Example, Sinatra Edition
-# A simple app that allows a user to first authorize their Dropbox account, and then upload a file to their Dropbox.
-
 get '/' do
-  if params[:oauth_token] then
-    dropbox_session = Dropbox::Session.deserialize(session[:dropbox_session])
-    dropbox_session.authorize(params)
-    session[:dropbox_session] = dropbox_session.serialize # re-serialize the authenticated session
-    redirect '/upload'
+  if not params[:oauth_token] then
+    dbsession = DropboxSession.new('ribh7ft60gym2l8', 'sj8rz89ril4wl76') # key, secret
+    session[:dropbox_session] = dbsession.serialize #serialize and save this DropboxSession
+    #pass to get_authorize_url a callback url that will return the user here
+    # redirect dbsession.get_authorize_url url_for(:action => 'authorize')
+    redirect dbsession.get_authorize_url("http://127.0.0.1:9393")
   else
-    dropbox_session = Dropbox::Session.new('key', 'secret')
-    session[:dropbox_session] = dropbox_session.serialize
-    redirect dropbox_session.authorize_url(:oauth_callback => 'http://localhost:9393')
+    # the user has returned from Dropbox
+    dbsession = DropboxSession.deserialize(session[:dropbox_session])
+    dbsession.get_access_token  #we've been authorized, so now request an access_token
+    session[:dropbox_session] = dbsession.serialize
+
+    redirect '/upload'
   end
 end
 
 get '/upload' do
- redirect '/' unless session[:dropbox_session]
- dropbox_session = Dropbox::Session.deserialize(session[:dropbox_session])
- dropbox_session.mode = :dropbox
- redirect '/' unless dropbox_session.authorized?
- dropbox_session.upload 'testfile.txt', 'Folder'
- uploaded_file = dropbox_session.file 'Folder/testfile.txt'
- 'This is the metadata: ' + uploaded_file.metadata.size
+  # Check if user has no dropbox session...re-direct them to authorize
+  redirect '/' unless session[:dropbox_session]
+  dbsession = DropboxSession.deserialize(session[:dropbox_session])
+  client = DropboxClient.new(dbsession, :app_folder) #raise an exception if session not authorized
+  info = client.account_info # look up account information
+
+  if request.method != "POST"
+    # show a file upload page
+    render :inline =>
+      "#{info['email']} <br/><%= form_tag({:action => :upload}, :multipart => true) do %><%= file_field_tag 'file' %><%= submit_tag %><% end %>"
+    return
+  else
+    # upload the posted file to dropbox keeping the same name
+    resp = client.put_file(params[:file].original_filename, params[:file].read)
+    render :text => "Upload successful! File now at #{resp['path']}"
+  end
 end
+
+# dropbox_session.mode = :dropbox
+ # redirect '/' unless dropbox_session.authorized?
+ # dropbox_session.upload 'testfile.txt', 'Folder'
+ # uploaded_file = dropbox_session.file 'Folder/testfile.txt'
+ # 'This is the metadata: ' + uploaded_file.metadata.size
 
 get "/logout" do
   session = {}
