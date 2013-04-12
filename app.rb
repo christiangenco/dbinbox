131b48937a680d552df006722d2d3d51a6705e0c
Christian Genco <christian.genco@gmail.com>
2013-04-12 15:47:22 -0500

    more logging to figure out why the url is wrong...

--- app.rb
+++ app.rb
@@ -70,6 +70,8 @@ end
 # if exists, look up access token and use that
 
 get '/' do
+  @@log.info request.base_url
+  @@log.info url('/')
   if !params[:oauth_token]
     @@log.info "first-time user!"
     haml :index
